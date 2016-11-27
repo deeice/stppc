0cd59b5d87926e0bccf45e36ecd234b5b62f876c
deeice <dheyse@hotmail.com>
2016-11-26 19:13:17 -0500

    Initial commit from the dingux port.

---
+++ src/version.c
@@ -0,0 +1,18 @@
+/*
+ * Puzzles version numbering.
+ */
+
+#define STR1(x) #x
+#define STR(x) STR1(x)
+
+#if defined REVISION
+
+char ver[] = "Revision: r" STR(REVISION);
+
+#else
+
+char ver[] = "Unknown version";
+
+#endif
+
+char stppc2x_ver[] = "STPPC Dingux release 1";
